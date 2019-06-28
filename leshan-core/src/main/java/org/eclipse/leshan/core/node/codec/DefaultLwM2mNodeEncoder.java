6861197fb0ff7a6c76e8cbd81ec1775ce9567380
Simon Bernard <sbernard@sierrawireless.com>
2019-06-28 16:58:16 +0200

    Do not do type conversion at encoding time by default anymore.

--- leshan-core/src/main/java/org/eclipse/leshan/core/node/codec/DefaultLwM2mNodeEncoder.java
+++ leshan-core/src/main/java/org/eclipse/leshan/core/node/codec/DefaultLwM2mNodeEncoder.java
@@ -52,7 +52,7 @@ public class DefaultLwM2mNodeEncoder implements LwM2mNodeEncoder {
      * Create {@link DefaultLwM2mNodeEncoder} without support of old TLV and JSON code.
      */
     public DefaultLwM2mNodeEncoder() {
-        this(new DefaultLwM2mValueConverter());
+        this(new NoValueConverter());
     }
 
     public DefaultLwM2mNodeEncoder(LwM2mValueConverter converter) {
@@ -68,7 +68,7 @@ public class DefaultLwM2mNodeEncoder implements LwM2mNodeEncoder {
      * @param supportDeprecatedContentFormat True to accept to encode old code.
      */
     public DefaultLwM2mNodeEncoder(boolean supportDeprecatedContentFormat) {
-        this(new DefaultLwM2mValueConverter(), supportDeprecatedContentFormat);
+        this(new NoValueConverter(), supportDeprecatedContentFormat);
     }
 
     public DefaultLwM2mNodeEncoder(LwM2mValueConverter converter, boolean supportDeprecatedContentFormat) {
